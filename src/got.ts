f8d963557c6f85e47755d957ed0376a6527151ff
Dominik Kundel <dkundel@twilio.com>
2020-08-27 10:20:48 -0700

    chore: deprecate repo and update readme

--- src/got.ts
+++
@@ -1,9 +0,0 @@
-import gotModule from 'got';
-
-// Got has retry logic that we don't want to use in tests
-export const got =
-  process.env.NODE_ENV === 'test'
-    ? gotModule.extend({ retry: { limit: 0 } })
-    : gotModule;
-
-export default got;
--- src/utils/sleep.ts
+++
@@ -1,8 +0,0 @@
-/** @module @twilio-labs/serverless-api/dist/utils */
-
-/**
- * Resolves after a given amount of milliseconds
- * @param  {number} ms milliseconds to wait
- */
-export const sleep = (ms: number) =>
-  new Promise(resolve => setTimeout(resolve, ms));
